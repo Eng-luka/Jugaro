60eacdfbd84b6d40078ad40e4b19acf252353ccc
luka127 <55964116+luka127@users.noreply.github.com>
2024-04-15 08:58:02 +0200

    Gaming Site

---
+++ main.js
@@ -0,0 +1,20 @@
+let btn = document.getElementById('btn');
+
+window.onscroll = function(){
+    if(scrollY >= 600)
+    {
+        btn.style.display = "block";
+    }
+    else{
+        btn.style.display = "none";
+    }
+}
+btn.onclick =function(){
+    scroll({
+        left:0,
+        top:0,
+        behavior:'smooth'
+    })
+}
+
+
