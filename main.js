8c01fa54bc8f33a222651705fcf390f061e2e926
luka127 <55964116+luka127@users.noreply.github.com>
2024-05-12 06:12:09 +0300

    New Update

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 let btn = document.getElementById('btn');
+let Support = document.getElementById('Support');
+let nav = document.getElementById('nav');
+let dialog =document.getElementById('dialog');
+let blurer = document.getElementById('blurer');
 
+// Btn Scroll 
 window.onscroll = function(){
     if(scrollY >= 600)
     {
@@ -16,5 +21,53 @@ btn.onclick =function(){
         behavior:'smooth'
     })
 }
+
+// Remove Support Div
+window.onscroll = function(){
+    if(scrollY >= 50)
+    {
+        Support.style.display = "none";
+    }
+    else{
+        Support.style.display = "block";
+    }
+}
+
+
+// Join us btn
+function openDialog() {
+    dialog.style.display ="flex";
+    blurer.style.display ="block";
+}
+function closeDialog() {
+    dialog.style.display="none";
+    blurer.style.display="none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for(const i of pop){
+    i.addEventListener("click", function(){
+    box.classList.toggle('active');
+    overlay.classList.toggle('active');
+}
+)};
 
 
